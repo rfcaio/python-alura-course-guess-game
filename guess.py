35d59cbf21feea554b2f4c5d15327fd18f330a58
Caio Rodrigues <rfcaio90@gmail.com>
2019-06-28 13:57:11 -0300

    py1-aula-04

--- guess.py
+++ guess.py
@@ -7,18 +7,29 @@ def is_greater_than(guess, secret):
     return guess > secret
 
 
+def show_status(turn, turns):
+    return 'TURN {} OF {}'.format(turn, turns)
+
+
 def init():
     secret = 47
-
+    turn = 1
+    turns = 3
     print('\n***** WELCOME TO THE GUESS GAME *****\n')
 
-    guess = input('ENTER A NUMBER: ')
+    while (turn <= turns):
+        print('\n***** {} ******\n'.format(show_status(turn, turns)))
 
-    if (equals(secret, int(guess))):
-        print('\nYES! YOU HIT THE NUMBER.\n')
-    else:
-        print('\nYOU DID NOT HIT THE SECRET NUMBER.')
-        if (is_greater_than(int(guess), secret)):
-            print('{} IS GREATER THAN SECRET NUMBER.'.format(guess))
+        guess = input('ENTER A NUMBER: ')
+
+        if (equals(secret, int(guess))):
+            print('\nYES! YOU HIT THE NUMBER.')
+            break
         else:
-            print('{} IS LOWER THAN SECRET NUMBER.'.format(guess))
+            turn += 1
+            print('\nYOU DID NOT HIT THE SECRET NUMBER.')
+            if (is_greater_than(int(guess), secret)):
+                print('{} IS GREATER THAN SECRET NUMBER.'.format(guess))
+            else:
+                print('{} IS LOWER THAN SECRET NUMBER.'.format(guess))
+    print('\n***** THE GAME IS OVER. *****\n')
--- test_guess.py
+++ test_guess.py
@@ -1,13 +1,24 @@
 from unittest import TestCase
 
-from guess import equals, is_greater_than
+from guess import equals, is_greater_than, show_status
+
 
 class TestGuess(TestCase):
-    def test_equals(self):
-        self.assertEqual(equals(4, 7), False)
+    def test_equals_should_return_true_when_the_first_number_is_equal_to_second_number(self):
         self.assertEqual(equals(7, 7), True)
 
 
-    def test_is_greater_than(self):
-        self.assertEqual(is_greater_than(4, 7), False)
+    def test_equals_should_return_false_when_the_first_number_is_not_equal_to_second_number(self):
+        self.assertEqual(equals(4, 7), False)
+
+
+    def test_is_greater_than_should_return_true_when_the_first_number_is_greater_than_the_second_number(self):
         self.assertEqual(is_greater_than(11, 7), True)
+
+
+    def test_is_greater_than_should_return_false_when_the_first_number_is_not_greater_than_the_second_number(self):
+        self.assertEqual(is_greater_than(4, 7), False)
+
+
+    def test_show_status_should_return_a_string_with_the_current_game_round(self):
+        self.assertEqual(show_status(3, 7), 'TURN 3 OF 7')
