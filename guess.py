2e7d802fb61f36e229af3b9570414ea86327d7c4
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-02 12:44:59 -0300

    py1-aula-05

--- guess.py
+++ guess.py
@@ -7,29 +7,35 @@ def is_greater_than(guess, secret):
     return guess > secret
 
 
+def is_out_of_range(guess, start = 1, end = 100):
+    return guess < start or guess > end
+
+
 def show_status(turn, turns):
     return 'TURN {} OF {}'.format(turn, turns)
 
 
 def init():
     secret = 47
-    turn = 1
     turns = 3
-    print('\n***** WELCOME TO THE GUESS GAME *****\n')
+    print('\n***** WELCOME TO THE GUESS GAME\n')
 
-    while (turn <= turns):
-        print('\n***** {} ******\n'.format(show_status(turn, turns)))
+    for turn in range(1, turns + 1):
+        print('\n***** {}\n'.format(show_status(turn, turns)))
 
-        guess = input('ENTER A NUMBER: ')
+        guess = input('ENTER A NUMBER BETWEEN 1 AND 100: ')
+
+        if (is_out_of_range(int(guess))):
+            print('\nNUMBER OUT OF RANGE!!!')
+            continue
 
         if (equals(secret, int(guess))):
             print('\nYES! YOU HIT THE NUMBER.')
             break
         else:
-            turn += 1
             print('\nYOU DID NOT HIT THE SECRET NUMBER.')
             if (is_greater_than(int(guess), secret)):
                 print('{} IS GREATER THAN SECRET NUMBER.'.format(guess))
             else:
                 print('{} IS LOWER THAN SECRET NUMBER.'.format(guess))
-    print('\n***** THE GAME IS OVER. *****\n')
+    print('\n***** THE GAME IS OVER.\n')
--- test_guess.py
+++ test_guess.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from guess import equals, is_greater_than, show_status
+from guess import equals, is_greater_than, is_out_of_range, show_status
 
 
 class TestGuess(TestCase):
@@ -18,6 +18,19 @@ class TestGuess(TestCase):
 
     def test_is_greater_than_should_return_false_when_the_first_number_is_not_greater_than_the_second_number(self):
         self.assertEqual(is_greater_than(4, 7), False)
+
+
+    def test_is_out_of_range_should_return_false_when_the_number_is_in_the_range(self):
+        self.assertEqual(is_out_of_range(3, 1, 10), False)
+
+
+    def test_is_out_of_range_should_return_true_when_the_number_is_not_in_the_range(self):
+        self.assertEqual(is_out_of_range(-1, 1, 10), True)
+
+
+    def test_is_out_of_range_should_use_1_and_100_as_default_for_start_and_end_respectively(self):
+        self.assertEqual(is_out_of_range(0), True)
+        self.assertEqual(is_out_of_range(101), True)
 
 
     def test_show_status_should_return_a_string_with_the_current_game_round(self):
