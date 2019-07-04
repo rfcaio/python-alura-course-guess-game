98131cbc6dd009c5f13517cb957f2c5cad2b92c0
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-04 13:03:50 -0300

    py1-aula-07

--- guess.py
+++ guess.py
@@ -4,6 +4,15 @@ def equals(guess, secret):
     return secret == guess
 
 
+def get_quantity_of_turns(difficulty_level):
+    if (difficulty_level == 1):
+        return 20
+    elif (difficulty_level == 2):
+        return 10
+    else:
+        return 5
+
+
 def is_greater_than(guess, secret):
     return guess > secret
 
@@ -16,27 +25,41 @@ def show_status(turn, turns):
     return 'TURN {} OF {}'.format(turn, turns)
 
 
+def update_points(points, guess, secret):
+    return points - abs(guess - secret)
+
+
 def init():
+    points = 1000
     secret = randrange(1, 101)
-    turns = 3
+    turns = 0
     print('\n***** WELCOME TO THE GUESS GAME\n')
+
+    print('***** (1) EASY')
+    print('***** (2) MEDIUM')
+    print('***** (3) HARD')
+    difficulty = int(input('\n***** CHOOSE YOUR DESTINY '))
+
+    turns = get_quantity_of_turns(difficulty)
 
     for turn in range(1, turns + 1):
         print('\n***** {}\n'.format(show_status(turn, turns)))
 
-        guess = input('ENTER A NUMBER BETWEEN 1 AND 100: ')
+        guess = int(input('ENTER A NUMBER BETWEEN 1 AND 100: '))
 
-        if (is_out_of_range(int(guess))):
+        if (is_out_of_range(guess)):
             print('\nNUMBER OUT OF RANGE!!!')
             continue
 
-        if (equals(secret, int(guess))):
+        if (equals(secret, guess)):
             print('\nYES! YOU HIT THE NUMBER.')
+            print('\nYOU SCORED {} POINTS.'.format(points))
             break
         else:
             print('\nYOU DID NOT HIT THE SECRET NUMBER.')
-            if (is_greater_than(int(guess), secret)):
+            if (is_greater_than(guess, secret)):
                 print('{} IS GREATER THAN SECRET NUMBER.'.format(guess))
             else:
                 print('{} IS LOWER THAN SECRET NUMBER.'.format(guess))
+            points = update_points(points, guess, secret)
     print('\n***** THE GAME IS OVER.\n')
--- test_guess.py
+++ test_guess.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from guess import equals, is_greater_than, is_out_of_range, show_status
+from guess import equals, get_quantity_of_turns, is_greater_than, is_out_of_range, show_status, update_points
 
 
 class TestGuess(TestCase):
@@ -10,6 +10,20 @@ class TestGuess(TestCase):
 
     def test_equals_should_return_false_when_the_first_number_is_not_equal_to_second_number(self):
         self.assertEqual(equals(4, 7), False)
+
+
+    def test_get_quantity_of_turns_should_return_20_when_1_is_passed_as_parameter(self):
+        self.assertEqual(get_quantity_of_turns(1), 20)
+
+
+    def test_get_quantity_of_turns_should_return_10_when_2_is_passed_as_parameter(self):
+        self.assertEqual(get_quantity_of_turns(2), 10)
+
+
+    def test_get_quantity_of_turns_should_return_5_when_another_number_is_passed_as_parameter(self):
+        self.assertEqual(get_quantity_of_turns(-1), 5)
+        self.assertEqual(get_quantity_of_turns(0), 5)
+        self.assertEqual(get_quantity_of_turns(3), 5)
 
 
     def test_is_greater_than_should_return_true_when_the_first_number_is_greater_than_the_second_number(self):
@@ -35,3 +49,8 @@ class TestGuess(TestCase):
 
     def test_show_status_should_return_a_string_with_the_current_game_round(self):
         self.assertEqual(show_status(3, 7), 'TURN 3 OF 7')
+
+
+    def test_update_points_should_return_the_amount_of_points_minus_the_difference_between_secret_number_and_guess_number(self):
+        self.assertEqual(update_points(100, 75, 50), 75)
+        self.assertEqual(update_points(100, 25, 50), 75)
