3591640bafde02df7f425faa020c84e8fdba3de3
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-08 13:03:54 -0300

    py2-aula-01

---
+++ hangman.py
@@ -0,0 +1,11 @@
+
+def init():
+    has_hanged = False
+    has_won = False
+
+    while (not has_hanged and not has_won):
+        print('\n***** WELCOME TO THE HANGMAN GAME\n')
+
+
+if (__name__ == '__main__'):
+    init()
--- init.py
+++ init.py
@@ -1,4 +1,15 @@
 
-from guess import init
+import guess
+import hangman
 
-init()
+print('\n***** (1) GUESS GAME')
+print('***** (2) HANGMAN GAME')
+
+game_option = int(input('\n***** CHOOSE YOUR GAME '))
+
+if (game_option == 1):
+    guess.init()
+elif (game_option == 2):
+    hangman.init()
+else:
+    print('\n***** INVALID OPTION!')
