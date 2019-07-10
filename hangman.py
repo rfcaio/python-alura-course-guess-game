e31016ea740f77ee6f0f7f4d8307cf09f14d0389
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-10 12:31:31 -0300

    py2-aula-03

--- hangman.py
+++ hangman.py
@@ -6,17 +6,21 @@ def sanitize(word):
 def init():
     has_hanged = False
     has_won = False
+    score = ['_', '_', '_', '_', '_', '_', '_', '_']
     secret = 'scorpion'
 
     print('\n***** WELCOME TO THE HANGMAN GAME\n')
+
+    print('***** SCORE: {}'.format(score))
 
     while (not has_hanged and not has_won):
         guess = sanitize(input('***** CHOOSE A LETTER '))
         index = 0
         for letter in secret:
             if (letter == guess):
-                print('***** FOUND {} IN INDEX {}'.format(letter, index))
+                score[index] = guess
             index += 1
+        print('\n***** SCORE: {}'.format(score))
 
 
 if (__name__ == '__main__'):
