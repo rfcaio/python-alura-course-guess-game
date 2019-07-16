e7d3d2a87dff5671275f90ed610bf8a62a4f6eae
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-16 13:20:33 -0300

    py2-aula-05

--- hangman.py
+++ hangman.py
@@ -1,27 +1,40 @@
 
+def has_hanged(errors):
+    return errors >= 5
+
+
+def has_won(score):
+    return '_' not in score
+
+
 def sanitize(word):
     return word.strip().lower()
 
 
 def init():
-    has_hanged = False
-    has_won = False
-    score = ['_', '_', '_', '_', '_', '_', '_', '_']
-    secret = 'scorpion'
+    errors = 0
+    secret = 'ermac'
+    score = ['_' for word in secret]
 
     print('\n***** WELCOME TO THE HANGMAN GAME\n')
 
     print('***** SCORE: {}'.format(score))
 
-    while (not has_hanged and not has_won):
+    while (not has_hanged(errors) and not has_won(score)):
+        print('***** {} ERROR{} LEFT'.format(5 - errors, 'S' if errors < 4 else ''))
         guess = sanitize(input('***** CHOOSE A LETTER '))
-        index = 0
-        for letter in secret:
-            if (letter == guess):
-                score[index] = guess
-            index += 1
+
+        if (guess in secret):
+            index = 0
+            for letter in secret:
+                if (letter == guess):
+                    score[index] = guess
+                index += 1
+        else:
+            errors += 1
         print('\n***** SCORE: {}'.format(score))
 
+    print('***** YOU WON THE GAME') if has_won(score) else print('***** YOU LOST THE GAME')
 
 if (__name__ == '__main__'):
     init()
--- test_hangman.py
+++ test_hangman.py
@@ -1,9 +1,27 @@
 from unittest import TestCase
 
-from hangman import sanitize
+from hangman import has_hanged, has_won, sanitize
 
 
 class TestHangman(TestCase):
+    def test_has_hanged_should_return_false_when_the_amount_of_erros_is_lower_than_five(self):
+        for error in range(0, 5):
+            self.assertEqual(has_hanged(error), False)
+
+
+    def test_has_hanged_should_return_true_when_the_amount_of_erros_is_greater_than_or_equals_five(self):
+        self.assertEqual(has_hanged(5), True)
+        self.assertEqual(has_hanged(6), True)
+
+
+    def test_has_won_should_return_false_when_underscore_is_present_in_the_score(self):
+        self.assertEqual(has_won(['j', '_', 'x']), False)
+
+
+    def test_has_won_should_return_true_when_underscore_is_not_present_in_the_score(self):
+        self.assertEqual(has_won(['j', 'a', 'x']), True)
+
+
     def test_sanitize_should_return_a_string_in_lower_case_with_no_blank_spaces(self):
         self.assertEqual(sanitize(' Foo '), 'foo')
         self.assertEqual(sanitize('  Foo'), 'foo')
